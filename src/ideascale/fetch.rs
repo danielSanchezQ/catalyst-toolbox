d1fb40d4cea590b24a583e57a62b2fe419255a5e
danielsanchezq <daniel.sanchez@iohk.io>
2021-08-17 13:53:06 +0200

    Added error report from ideascale error reporting

--- src/ideascale/fetch.rs
+++ src/ideascale/fetch.rs
@@ -11,10 +11,22 @@ use std::convert::TryInto;
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
-    RequestError(#[from] reqwest::Error),
+    Request(#[from] reqwest::Error),
 
     #[error("Could not get value from json, missing attribute {attribute_name}")]
     MissingAttribute { attribute_name: &'static str },
+
+    #[error("Error deserializing proposal: {error}\n{data}")]
+    ProposalDeserialization {
+        error: serde_json::Error,
+        data: String,
+    },
+
+    #[error("Ideascale api replied with, code {key}: {message}")]
+    IdescaleApi {
+        key: String,
+        message: String,
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -27,6 +39,12 @@ struct Score {
 
 pub type Scores = HashMap<u32, f32>;
 
+#[derive(Debug, Deserialize)]
+struct IdeascaleError {
+    message: Option<String>,
+    key: String
+}
+
 static BASE_IDEASCALE_URL: Lazy<url::Url> = Lazy::new(|| {
     "https://cardano.ideascale.com/a/rest/v1/"
         .try_into()
@@ -36,14 +54,21 @@ static BASE_IDEASCALE_URL: Lazy<url::Url> = Lazy::new(|| {
 static CLIENT: Lazy<reqwest::Client> = Lazy::new(reqwest::Client::new);
 
 async fn request_data<T: DeserializeOwned>(api_token: String, url: Url) -> Result<T, Error> {
-    CLIENT
+    let response = CLIENT
         .get(url)
         .header("api_token", api_token)
         .send()
-        .await?
-        .json()
-        .await
-        .map_err(Error::RequestError)
+        .await?;
+
+    if response.status() != http::status::StatusCode::OK {
+        let ideascale_error_report: IdeascaleError = response.json().await?;
+        Err(Error::IdescaleApi {
+            key: ideascale_error_report.key,
+            message: ideascale_error_report.message.unwrap_or_default(),
+        })
+    } else {
+        response.json().await.map_err(Error::Request)
+    }
 }
 
 pub async fn get_funds_data(api_token: String) -> Result<Vec<Fund>, Error> {
@@ -73,7 +98,7 @@ pub async fn get_proposals_data(
     challenge_id: u32,
     api_token: String,
 ) -> Result<Vec<Proposal>, Error> {
-    request_data(
+    let json_values: Vec<serde_json::Value> = request_data(
         api_token,
         BASE_IDEASCALE_URL
             // ideascale API have some pager system which is not easy to find in the documentation
@@ -82,7 +107,17 @@ pub async fn get_proposals_data(
             .join(&format!("campaigns/{}/ideas/0/100000", challenge_id))
             .unwrap(),
     )
-    .await
+    .await?;
+    let mut proposals = Vec::new();
+    for value in json_values {
+        proposals.push(serde_json::from_value(value.clone()).map_err(|e| {
+            Error::ProposalDeserialization {
+                error: e,
+                data: serde_json::to_string_pretty(&value).unwrap(),
+            }
+        })?);
+    }
+    Ok(proposals)
 }
 
 pub async fn get_funnels_data_for_fund(api_token: String) -> Result<Vec<Funnel>, Error> {
